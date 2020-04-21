8ac20ad22676b1be15438348bf1de3d56fb49852
zahidul islam <zahidulislam5594@gmail.com>
2020-04-21 20:11:59 +0600

    Add project files.

---
+++ EncryptionWithoutSpecialCharacter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionWithoutSpecialCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the value: ");
+            string a = Console.ReadLine();
+            string encryp= CryptoEngine.ConvertStringToHex(a);
+            string decryp = CryptoEngine.ConvertHexToString(encryp);
+            Console.WriteLine(encryp);
+            Console.WriteLine(decryp);
+        }
+    }
+}
